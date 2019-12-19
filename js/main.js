da3823b106ad3f824cb70451bbfb4796a94af47e
AR-Arup <kumararup977@gmail.com>
2019-12-19 22:45:56 +0600

    update to

--- js/main.js
+++ js/main.js
@@ -1,50 +1,47 @@
 /*  ################### 
        ! Preloader
     ###################  */
-    $(window).on('load',function () {
-        $("#status").fadeOut();
-        $("#preloader").delay(350).fadeOut(500);
-    });
+$(window).on('load', function () {
+    $("#status").fadeOut();
+    $("#preloader").delay(350).fadeOut(500);
+});
+// ! mobile menu click option
 
+function openNav() {
+    document.getElementById("myNav").style.width = "100%";
+}
 
-   
-/*  ################### 
-         ! STICKY MENU
-    ###################  */
-   // $(document).ready(function (){
-        
-
-       //  $(".js-about-us").waypoint(function(direction){
-     //       if (direction == "down"){
-       //         $("nav").addClass("sticky");
-     //       }
-     //       else {
-          //      $("nav").removeClass("sticky");
-      //      }
-  //       });
- //   });
+function closeNav() {
+    document.getElementById("myNav").style.width = "0%";
+}
 
 
 
-    
-
-    // ! mobile menu click option
-
-    function openNav () {
-        document.getElementById("myNav").style.width = "100%";
-    }
-
-    function closeNav () {
-        document.getElementById("myNav").style.width = "0%";
-    }
-
-
-    
 /*  ############################### 
       ? ((...PORTFOLIO...))
-  
     ###############################  */
-
-   $(document).ready(function (){
+$(document).ready(function () {
     var mixer = mixitup('.portfolio-gallery');
-   });
+});
+
+
+
+/*  ############################### 
+    ? ((...HIDE SHOW BACK BUTTON...))
+    ###############################  */
+$(function () {
+    $(window).scroll(function () {
+        // ! Show/hide page loade
+        showHideNav();
+    });
+
+    function showHideNav() {
+        if ($(window).scrollTop() > 50) {
+            // ! Show Back To right Button
+            $("#back-to-top").fadeIn();
+        } else {
+            // ! Hide Back To right Button
+            $("#back-to-top").fadeOut();
+        }
+    }
+});
